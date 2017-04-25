c6fba291ce61b31910b4c801b6a9b932f49fa947
penguinfreud <qiyiwsy@yahoo.com>
2017-04-26 01:53:03 +0800

    init

---
+++ src/test/java/edu/fudan/NeocommerceApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.fudan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NeocommerceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
