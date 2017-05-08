7eaf3d9bcd2147966a84ce8f2a673d9e432014dd
NeoZhouz <574560777@qq.com>
2017-05-08 22:39:20 +0800

    Update nav-bar.component.ts

--- frontend/src/app/nav-bar.component.ts
+++ frontend/src/app/nav-bar.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'nav-bar',
     templateUrl: './nav-bar.component.html'
+    styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent {}
